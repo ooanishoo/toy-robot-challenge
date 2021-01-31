83c47c46d688981dc0afb8d5f09db06d80f518a8
ooanishoo <anish.maharjan12341@gmail.com>
2021-01-31 18:34:16 +1100

    Write unit test for parseDirection when direction is valid and invalid

---
+++ src/direction/direction.spec.ts
@@ -0,0 +1,27 @@
+import { parseDirection } from './direction';
+
+describe('parseDirection', () => {
+
+  describe('When a direction is valid', () => {
+    test.each`
+      text       | expected
+      ${'EAST'}  | ${'EAST'}
+      ${'WEST'}  | ${'WEST'}
+      ${'NORTH'} | ${'NORTH'}
+      ${'SOUTH'} | ${'SOUTH'}
+  `('should parse text $text to type $expected', ({ text, expected }) => {
+      const result = parseDirection(text);
+      expect(result).toBe(expected);
+    });
+  });
+
+
+  describe('When a direction is invalid', () => {
+    const input = ['NORTHERN', 'SOUTHERN', 'WESTERMN', '', '-', '_', null, undefined];
+    test.each(input)('should throw error when text = %p', (text) => {
+      expect(() => parseDirection(text)).toThrowError(`Invalid Direction: ${text}`);
+    });
+  });
+
+});
+
