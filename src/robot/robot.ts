2d5e105edf4753be663d6be77ba461365b15d456
ooanishoo <anish.maharjan12341@gmail.com>
2021-01-31 18:50:08 +1100

    Add left, right, move and run method

--- src/robot/robot.ts
+++ src/robot/robot.ts
@@ -1,7 +1,6 @@
 import { Direction } from '../direction/direction';
 import { Command } from '../command/command';
 import { Table } from '../table/table';
-import * as logger from '../utils/logger'
 import { processNextMove } from '../utils/processNextMove';
 import { turnLeft, turnRight } from '../rotation/rotation';
 
@@ -37,6 +36,54 @@ export class Robot {
     this._y = y;
     this._direction = direction;
     this._isPlaced = true;
+  }
+
+  public move(): void {
+    if (!this._isPlaced) {
+      return;
+    }
+    const { x, y } = processNextMove(this._direction);
+    if (!this._table.isWithinRange(x + this._x, y + this._y)) {
+      return;
+    }
+    this._x += x;
+    this._y += y;
+
+  }
+
+  public left(): void {
+    if (!this._isPlaced) {
+      return;
+    }
+    this._direction = turnLeft(this._direction);
+  }
+
+  public right(): void {
+    if (!this._isPlaced) {
+      return;
+    }
+    this._direction = turnRight(this._direction);
+  }
+
+
+  public run(command: Command): void {
+    switch (command.type) {
+      case 'PLACE':
+        this.place(command.x, command.y, command.direction);
+        break;
+      case 'LEFT':
+        this.left();
+        break;
+      case 'RIGHT':
+        this.right();
+        break;
+      case 'MOVE':
+        this.move();
+        break;
+      case 'REPORT':
+        this.report();
+        break;
+    }
   }
 
 }
