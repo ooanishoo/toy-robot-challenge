a238f7e91b52154c6d1f4f557625f8b2482b694f
ooanishoo <anish.maharjan12341@gmail.com>
2021-01-31 18:45:40 +1100

    Unit test for isWithinRange()

--- src/table/table.test.ts
+++ src/table/table.test.ts
@@ -17,4 +17,52 @@ describe(`Table Test`, () => {
     });
   });
 
+  describe('isWithinRange()', () => {
+    const width = 3;
+    const height = 3;
+    const table = new Table(width, height);
+    describe(`When a table is of (${table.width} x ${table.height}) dimesnsion:`, () => {
+      test.each`
+        x     | y 
+        ${0}  | ${0}
+        ${0}  | ${1}
+        ${0}  | ${2}
+        ${1}  | ${0}
+        ${1}  | ${1}
+        ${1}  | ${2}
+        ${2}  | ${0}
+        ${2}  | ${1}
+        ${2}  | ${2}
+      `(`(x=$x, y=$y) should be within the boundary of the table`, ({ x, y }) => {
+        const result = table.isWithinRange(x, y);
+        expect(result).toBe(true);
+      });
+
+      test.each`
+        x      | y 
+        ${0}   | ${-1}
+        ${-1}  | ${0}
+        ${2}   | ${-1}
+        ${3}   | ${0}
+        ${2}   | ${3}
+        ${3}   | ${2}
+        ${-1}  | ${2}
+        ${0}   | ${3}
+      `(`(x=$x, y=$y) should NOT be within the boundary of the table`, ({ x, y }) => {
+        const result = table.isWithinRange(x, y);
+        expect(result).toBe(false);
+      });
+    });
+  })
 });
+
+/*
+  ${0}   | ${-1}
+${-1}  | ${0}
+${2}   | ${-1}
+${3}   | ${0}
+${2}   | ${3}
+${3}   | ${2}
+${-1}  | ${2}
+${0}   | ${3}
+*/
